d1018cd2f5c06dd9f4dfa55be7ac380a34f3b641
Andy <tty09183@gmail.com>
2020-11-20 18:57:38 +0800

    登入註冊

---
+++ app/src/main/java/com/example/trattention/login.java
@@ -0,0 +1,25 @@
+package com.example.trattention;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+public class login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        //設定隱藏標題
+        getSupportActionBar().hide();
+        TextView textview_ForgetPassword = (TextView) findViewById(R.id.ForgetPassword);
+
+        textview_ForgetPassword.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView textview_Registration = (TextView) findViewById(R.id.registration1);
+
+        textview_Registration.setMovementMethod(LinkMovementMethod.getInstance());
+
+    }
+
+}
---
+++ app/src/main/java/com/example/trattention/registration.java
@@ -0,0 +1,63 @@
+package com.example.trattention;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+public class registration extends AppCompatActivity {
+
+    private Spinner Spingender;
+    private Spinner Spinage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        //設定隱藏標題
+        getSupportActionBar().hide();
+
+
+        Spingender = (Spinner) findViewById(R.id.editTextTextPersonGender);
+
+        ArrayAdapter<CharSequence> arrAdapSpn
+                = ArrayAdapter.createFromResource(registration.this, //對應的Context
+                R.array.gender, //選項資料內容
+                android.R.layout.simple_spinner_item); //自訂getView()介面格式(Spinner介面未展開時的View)
+
+        arrAdapSpn.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); //自訂getDropDownView()介面格式(Spinner介面展開時，View所使用的每個item格式)
+        Spingender.setAdapter(arrAdapSpn); //將宣告好的 Adapter 設定給 Spinner
+        Spingender.setOnItemSelectedListener(spnRegionOnItemSelected);
+
+        Spinage = (Spinner) findViewById(R.id.editTextTextPersonAge);
+
+        ArrayAdapter<CharSequence> arrAdapSpn1
+                = ArrayAdapter.createFromResource(registration.this, //對應的Context
+                R.array.age, //選項資料內容
+                android.R.layout.simple_spinner_item); //自訂getView()介面格式(Spinner介面未展開時的View)
+
+        arrAdapSpn.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); //自訂getDropDownView()介面格式(Spinner介面展開時，View所使用的每個item格式)
+        Spinage.setAdapter(arrAdapSpn1); //將宣告好的 Adapter 設定給 Spinner
+        Spinage.setOnItemSelectedListener(spnRegionOnItemSelected);
+    }
+
+    private AdapterView.OnItemSelectedListener spnRegionOnItemSelected
+            = new AdapterView.OnItemSelectedListener()
+    {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View v, int position, long id)
+        {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg0)
+        {
+            // TODO Auto-generated method stub
+        }
+    };
+}
