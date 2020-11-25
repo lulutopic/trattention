6177b232f2535560890d8e90b6add3e0d8e8174e
Andy <tty09183@gmail.com>
2020-11-25 12:51:07 +0800

    完成遊戲選擇頁面，新建前導片頁面（香香要做的）

---
+++ app/src/main/java/com/example/trattention/MemoryGame.java
@@ -0,0 +1,14 @@
+package com.example.trattention;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemoryGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memory_game);
+    }
+}
--- app/src/main/java/com/example/trattention/login.java
+++ app/src/main/java/com/example/trattention/login.java
@@ -1,26 +1,46 @@
 package com.example.trattention;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class login extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         //設定隱藏標題
         getSupportActionBar().hide();
-        TextView textview_ForgetPassword = (TextView) findViewById(R.id.ForgetPassword);
 
-        textview_ForgetPassword.setMovementMethod(LinkMovementMethod.getInstance());
+        //註冊的文字跳轉
+        TextView re = (TextView) findViewById(R.id.registration1);
+        re.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View fp) {
+                Intent intent = new Intent();
+                intent.setClass(login.this, registration.class);
+                startActivity(intent);
+            }
+        });
 
-        TextView textview_Registration = (TextView) findViewById(R.id.registration1);
+        //忘記密碼的文字跳轉
+        TextView fp = (TextView) findViewById(R.id.ForgetPassword);
+        re.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(login.this, registration.class);
+                startActivity(intent);
+            }
+        });
 
-        textview_Registration.setMovementMethod(LinkMovementMethod.getInstance());
 
-        
     }
+
 
 }
