08643e549d79fdab04ba97d091b59c793ab641eb
m22104416 <63833300+m22104416@users.noreply.github.com>
2020-12-09 19:45:32 +0800

    個人化設置

--- app/src/main/java/com/example/trattention/personal.java
+++ app/src/main/java/com/example/trattention/personal.java
@@ -7,17 +7,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class personal extends AppCompatActivity {
 
+    Switch aSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
         //設定隱藏標題
         getSupportActionBar().hide();
+        //switch開關
+        aSwitch=(Switch)findViewById(R.id.switch1);
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+               if(isChecked == true){
+                   Toast.makeText(getBaseContext(),"On",Toast.LENGTH_SHORT).show();
+               }else{
+                   Toast.makeText(getBaseContext(),"OFF",Toast.LENGTH_SHORT).show();
+               }
 
+            }
+        });
 
 
     }
