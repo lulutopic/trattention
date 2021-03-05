aaa883dfa35ebd619408e76db0499573a5096d54
Andy <tty09183@gmail.com>
2021-03-05 12:58:23 +0800

    圖形配對

--- app/src/main/java/com/example/trattention/imagePair.java
+++ app/src/main/java/com/example/trattention/imagePair.java
@@ -1,16 +1,19 @@
 package com.example.trattention;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-import android.media.Image;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Chronometer;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.view.View;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
@@ -20,12 +23,21 @@ public class imagePair extends AppCompatActivity {
     private Chronometer timer; //已經過時間
     private Handler handler = new Handler();
 
-    ImageView img01,img02,img03,question;
-    LinearLayout optionA,optionB,optionC;
 
-    int[] quesArray = {R.drawable.red_green,R.drawable.blue_red,R.drawable.green_blue};
-    int[] ansArray = {R.drawable.red,R.drawable.green,R.drawable.blue};
-    Random random = new Random();
+    private ArrayList<String> colorNames = new ArrayList<>();
+    private ArrayList<Integer> colorValues = new ArrayList<>();
+    private ArrayList<TextView> button = new ArrayList<>();
+
+    private Random random;
+
+    private TextView colorTextView;
+
+    private TextView redImageButton;
+    private TextView blueImageButton;
+    private TextView greenImageButton;
+
+    int count = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,24 +52,125 @@ public class imagePair extends AppCompatActivity {
         handler.removeCallbacks(updateTimer);
         //設定Delay的時間
         handler.postDelayed(updateTimer, 10);
+        random = new Random();
+        populateBothArraylists();
+        setupViewsAndListeners();
+        getRandomColor();
+    }
 
-        optionA = (LinearLayout)findViewById(R.id.optionA);
-        optionB = (LinearLayout)findViewById(R.id.optionB);
-        optionC = (LinearLayout)findViewById(R.id.optionC);
-        img01 = (ImageView)findViewById(R.id.img01);
-        img02 = (ImageView)findViewById(R.id.img02);
-        img03 = (ImageView)findViewById(R.id.img03);
-        question = (ImageView) findViewById(R.id.question);
+    private void setupViewsAndListeners(){
 
-        ImageView[] imgArray = {img01,img02,img03};
+        redImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //Convert background to drawable.
+                ColorDrawable col = (ColorDrawable) v.getBackground();
 
-        Collections.shuffle(Arrays.asList(imgArray));
+                //Get color of drawable.
+                int color = col.getColor();
 
-        for (int i = 0 ; i < ansArray.length ; i++){
-            imgArray[i].setImageResource(ansArray[i]);
+                if(color == colorTextView.getCurrentTextColor()){
+                    count++;
+                    getRandomColor();
+                    checkEnd();
+                }
+            }
+        });
+        greenImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //Convert background to drawable.
+                ColorDrawable col = (ColorDrawable) v.getBackground();
+
+                //Get color of drawable.
+                int color = col.getColor();
+
+                if(color == colorTextView.getCurrentTextColor()){
+                    count++;
+                    getRandomColor();
+                    checkEnd();
+                }
+            }
+        });
+        blueImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //Convert background to drawable.
+                ColorDrawable col = (ColorDrawable) v.getBackground();
+
+                //Get color of drawable.
+                int color = col.getColor();
+
+                if(color == colorTextView.getCurrentTextColor()){
+                    count++;
+                    getRandomColor();
+                    checkEnd();
+                }
+            }
+        });
+    }
+
+    private void checkEnd(){
+        if(count == 3){
+            //頁面跳轉
+            Intent intent = new Intent();
+            intent.setClass(imagePair.this, home.class);
+            intent.putExtra("time",startTime);
+            startActivity(intent);
+            finish();
         }
+    }
+
+
+
+    private void populateBothArraylists(){
+        colorTextView = (TextView) findViewById(R.id.question);
+
+        redImageButton = (TextView) findViewById(R.id.optionA);
+        blueImageButton = (TextView)findViewById(R.id.optionB);
+        greenImageButton = (TextView) findViewById(R.id.optionC);
+
+        //add color names to the ArrayList
+        colorNames.add("Red");
+        colorNames.add("blue");
+        colorNames.add("green");
+
+        //Add color values to the arraylist
+        colorValues.add(ContextCompat.getColor(this,R.color.colorRed));
+        colorValues.add(ContextCompat.getColor(this,R.color.colorGreen));
+        colorValues.add(ContextCompat.getColor(this,R.color.colorBlue));
+
+        button.add(redImageButton);
+        button.add(blueImageButton);
+        button.add(greenImageButton);
+    }
+
+    private void getRandomColor(){
+
+        //Add a bit more randomness
+        Collections.shuffle(colorNames);
+        Collections.shuffle(colorValues);
+        Collections.shuffle(button);
+
+        //using a random number to get a random color from the arrayList
+        int randomColor = random.nextInt(colorNames.size());
+        String colorChosen = colorNames.get(randomColor);
+
+        //set this random color to be the text that is shown at the bottom
+        colorTextView.setText(colorChosen);
+
+        //chose an actual color to be randomly generated for the text
+        int randomColorTextColor1 = random.nextInt(colorValues.size());
+        int randomColorTextColor2 = random.nextInt(colorValues.size());
+        int randomColorTextColor3 = random.nextInt(colorValues.size());
+        colorTextView.setTextColor(colorValues.get(randomColorTextColor1));
+        button.get(0).setBackgroundColor(colorValues.get(randomColorTextColor1));
+        button.get(1).setBackgroundColor(colorValues.get(randomColorTextColor2));
+        button.get(2).setBackgroundColor(colorValues.get(randomColorTextColor3));
 
     }
+
+
 
     //固定要執行的方法
     private Runnable updateTimer = new Runnable() {
